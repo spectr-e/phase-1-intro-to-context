537342a07f1a921f419c7a5bff50f895a1b1c755
josiah <devs.josia@gmail.com>
2022-12-12 23:15:57 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-// Your code here
+function createEmployeeRecord(array) {
+  return {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+}
+
+function createEmployeeRecords(arrayNest) {
+  return arrayNest.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(record, time) {
+  const timeMod = time.split(" ");
+  record.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(timeMod[1]),
+    date: timeMod[0],
+  });
+  return record;
+}
+
+function createTimeOutEvent(record, time) {
+  const timeMod = time.split(" ");
+  record.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(timeMod[1]),
+    date: timeMod[0],
+  });
+  return record;
+}
+
+function hoursWorkedOnDate(record, date) {
+  for (let i of record.timeInEvents) {
+    for (let o of record.timeOutEvents) {
+      if (i.date === date && o.date === date) {
+        return (o.hour - i.hour) / 100;
+      }
+    }
+  }
+}
+
+function wagesEarnedOnDate(record, date) {
+  for (let i of record.timeInEvents) {
+    for (let o of record.timeOutEvents) {
+      if (i.date === date && o.date === date) {
+        return hoursWorkedOnDate(record, date) * record.payPerHour;
+      }
+    }
+  }
+}
+function allWagesFor(record) {
+  let total = 0;
+  for (let i of record.timeInEvents) {
+    for (let o of record.timeOutEvents) {
+      if (i.date === o.date) {
+        let date = i.date;
+        let pay = hoursWorkedOnDate(record, date) * record.payPerHour;
+        total += pay;
+      }
+    }
+  }
+  return total;
+}
+
+function calculatePayroll(records) {
+    return records.reduce((m, e) => m + allWagesFor(e), 0)
+}
